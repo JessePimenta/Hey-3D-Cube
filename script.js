163298fc6a638dac71e551afd5685bda546e4feb
Jesse Pimenta <drrrreams@gmail.com>
2016-08-17 02:39:11 -0600

    added temporary page

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+
+
 var $bg = $('#bg');
 
 $(function(){
   $bg.fadeIn(15000).fadeOut(10).fadeIn(20000).css('color','grey');
 console.log('bg delay started');
+
+
+
+var text = "Talk To You Soon";
+
+for(var i in text) {
+  if(text[i] === " ") {
+    $(".wavetext").append( $("<span>").html("&nbsp;") );
+  } else {
+    $(".wavetext").append( $("<span>").text(text[i]) );
+  }
+}
 })
