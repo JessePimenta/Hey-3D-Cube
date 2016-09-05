115c95e5e276c3aa924cb195defe580e4a2e6814
Jesse Pimenta <drrrreams@gmail.com>
2016-09-04 18:24:31 -0600

    added random dimensions generation to cube faces!!

--- script.js
+++ script.js
@@ -7,6 +7,24 @@ $(function(){
 console.log('bg delay started');
 
 
+$('.container').click(function(){
+  var self = $(this);
+  var Width = 600;
+  var Height = 600;
+
+  thisWidth = Math.floor(Math.random()*(Width)) + 1; //+1 to avoid 0px width
+  thisHeight = Math.floor(Math.random()*(Height)) + 1; //+1 to avoid 0px height
+
+  $('#n1, #n2, #n3, #n4, #n5, #n6').css({
+    "width":thisWidth,
+			"height":thisHeight,
+  })
+  $('.cube').css({
+    "width": thisWidth,
+    "height": thisHeight
+  })
+})
+
 
 var text = "Talk To You Soon";
 
